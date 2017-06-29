558cba4afcf74b9cc37a0e621e795bd38653bc80
RodrigoMarquesCampos <digoleeke@gmail.com>
2017-06-28 23:00:56 -0300

    Criação violação regra de negocios em Cliente.java

---
+++ Codigo Fonte/DSC.JROficina.Aplicacao/src/DSC/JROficina/Aplicacao/Cliente.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DSC.JROficina.Aplicacao;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class Cliente extends Pessoa{
+    private String cpf_cnpj;
+    private int tipo;
+
+    public Cliente(String nome, String telefone, int id, String endereco, String cpf_cnpj, int tipo) {
+        super(nome, telefone, id, endereco);
+        this.tipo = tipo;
+        this.cpf_cnpj = cpf_cnpj;
+    }
+    
+    public String getCpf_cnpj() {
+        return cpf_cnpj;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setCpf_cnpj(String cpf_cnpj) {
+        this.cpf_cnpj = cpf_cnpj;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     *
+     * @param nome
+     * @throws ViolacaoRegrasNegocioException
+     */
+    public void setNome(String nome) throws ViolacaoRegrasNegocioException{
+        if(nome == null)
+            throw new ViolacaoRegrasNegocioException("O nome do Cliente não pode ficar vazio!"); 
+    
+        
+        
+        char c;
+        int cont = 0;
+            
+        //Verificação de numeros e/ou caracteres especiais
+        for(int i =0; i<nome.length(); i++){
+            c = nome.charAt(i);
+            
+            if(((c >= 60) && (c <= 90)) || ((c >= 97) && (c<= 122)) || (c == 'á') || (c == 'â') || (c == 'ê') || (c == 'é') || (c == 'ó') || (c == 'ô')){
+              
+            }else{
+                cont++;
+            }    
+        }
+        
+        if(cont != 0)
+            throw new ViolacaoRegrasNegocioException("O nome do Cliente não pode ter numeros e/ou caracteres especiais!");
+        
+        this.nome = nome;
+    }
+
+    
+    
+
+}
