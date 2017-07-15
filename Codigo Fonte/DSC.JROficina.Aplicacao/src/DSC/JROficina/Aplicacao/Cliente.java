72be777de1d08eeb6614b2fecaf025d76c76941c
RodrigoMarquesCampos <digoleeke@gmail.com>
2017-07-14 21:12:12 -0300

    Alteração

--- Codigo Fonte/DSC.JROficina.Aplicacao/src/DSC/JROficina/Aplicacao/Cliente.java
+++ Codigo Fonte/DSC.JROficina.Aplicacao/src/DSC/JROficina/Aplicacao/Cliente.java
@@ -40,14 +40,14 @@ public class Cliente extends Pessoa{
      * @param nome
      * @throws ViolacaoRegrasNegocioException
      */
-    public void setNome(String nome) throws ViolacaoRegrasNegocioException{
+    public void setNomee(String nome) throws ViolacaoRegrasNegocioException{
         if(nome == null)
             throw new ViolacaoRegrasNegocioException("O nome do Cliente não pode ficar vazio!"); 
     
         
         
         char c;
-        int cont = 0;
+        int cont = nome.length();
             
         //Verificação de numeros e/ou caracteres especiais
         for(int i =0; i<nome.length(); i++){
@@ -57,19 +57,14 @@ public class Cliente extends Pessoa{
             || (c == 'â') || (c == 'ã') || (c == 'Á') || (c == 'Â') || (c == 'Ã')
             || (c == 'ê') || (c == 'é') || (c == 'É') || (c == 'Ê') || (c == 'ó')
             || (c == 'ô') || (c == 'õ') || (c == 'Ó') || (c == 'Ô') || (c == 'Õ')){
-              
-            }else{
-                cont++;
-            }    
-        }
+              cont++; 
+            }
         
-        if(cont != 0)
+        if(cont != nome.length())
             throw new ViolacaoRegrasNegocioException("O nome do Cliente não pode ter numeros e/ou caracteres especiais!");
         
         this.nome = nome;
+        }
     }
-
-    
-    
-
+        
 }
